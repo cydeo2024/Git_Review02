315d4369bd2e5e2c80b02f638b7be3c3f35e217a
Madina <mdavlatshoeva84@gmail.com>
2024-02-29 13:20:59 -0500

    made changes to firstClass

--- src/day01/firstClass.java
+++ src/day01/firstClass.java
@@ -1,7 +1,8 @@
 package day01;
 
 public class firstClass {
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
+        System.out.println("Hello World");
     }
 }
